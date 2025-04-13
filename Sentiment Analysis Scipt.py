f57605f4908da7704e5e5028de71e225d12b4abb
Nkeiruka Cynthia onyegwara <114313589+Conyegwara@users.noreply.github.com>
2025-04-13 20:47:19 +0100

    Run Script

---
+++ Sentiment Analysis Scipt.py
@@ -0,0 +1,191 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+
+=========== RESTART: C:\Users\onyeg\Downloads\python project\test2.py ==========
+                                               clean_text  category
+0       when modi promisedminimum government maximum g...      -1.0
+1       talk all the nonsense and continue all the dra...       0.0
+2       what did just say vote for modi  welcome bjp t...       1.0
+3       asking his supporters prefix chowkidar their n...       1.0
+4       answer who among these the most powerful world...       1.0
+...                                                   ...       ...
+162975  why these 456 crores paid neerav modi not reco...      -1.0
+162976  dear rss terrorist payal gawar what about modi...      -1.0
+162977  did you cover her interaction forum where she ...       0.0
+162978  there big project came into india modi dream p...       0.0
+162979  have you ever listen about like gurukul where ...       1.0
+
+[162980 rows x 2 columns]
+category
+ 1    44.332971
+ 0    33.878222
+-1    21.788806
+Name: proportion, dtype: float64
+[nltk_data] Downloading package stopwords to
+[nltk_data]     C:\Users\onyeg\AppData\Roaming\nltk_data...
+[nltk_data]   Package stopwords is already up-to-date!
+Model Accuracy: 0.73
+
+Classification Report:
+               precision    recall  f1-score   support
+
+          -1       0.88      0.44      0.58      7152
+           0       0.84      0.66      0.74     11067
+           1       0.65      0.93      0.77     14375
+
+    accuracy                           0.73     32594
+   macro avg       0.79      0.67      0.69     32594
+weighted avg       0.77      0.73      0.72     32594
+
+
+Warning (from warnings module):
+  File "C:\Users\onyeg\Downloads\python project\test2.py", line 74
+    sns.countplot(x=df['category'], palette="coolwarm")
+FutureWarning: 
+
+Passing `palette` without assigning `hue` is deprecated and will be removed in v0.14.0. Assign the `x` variable to `hue` and set `legend=False` for the same effect.
+
